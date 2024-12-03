537a3549ad58a77400c907c4e019a20882dcf002
Artur Yurchenko <artur24814@gmail.com>
2024-12-03 14:44:41 +0100

    components have been created to display DICOM metadata

--- frontend/src/consts/apiUrls.js
+++ frontend/src/consts/apiUrls.js
@@ -1,3 +1,4 @@
 export const LOGIN_API_URL = 'token/'
 export const REFRESH_API_TOKEN = 'token/refresh/'
 export const REGISTER_API_URL = 'auth/register/'
+export const DICOM_READ_API_URL = 'dicom/read/file/'
