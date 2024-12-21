bb24d96cc24609410d6770c09098bd6dd35c25e1
Artur Yurchenko <artur24814@gmail.com>
2024-12-21 17:57:01 +0100

    The MomentoContext has been integrated into DicomUpload page

--- frontend/src/pages/dicomUpload/components/ImageCanvasComponent.js
+++ frontend/src/pages/dicomUpload/components/ImageCanvasComponent.js
@@ -1,22 +1,8 @@
 import React, { useRef, useEffect } from 'react'
 import { Container } from 'react-bootstrap'
 
-const ImageCamvasComponent = ({ imageSrc, activeTool }) => {
+const ImageCamvasComponent = ({ imageSrc, activeTool, saveMemento, mementoName }) => {
   const canvasRef = useRef(null)
-
-  useEffect(() => {
-    if (imageSrc) {
-      const canvas = canvasRef.current
-      const ctx = canvas.getContext('2d')
-      const img = new Image()
-      img.src = imageSrc
-      img.onload = () => {
-        canvas.width = img.width
-        canvas.height = img.height
-        ctx.drawImage(img, 0, 0, img.width, img.height)
-      }
-    }
-  }, [imageSrc])
 
   const handleMouseDown = (event) => {
     if (activeTool) activeTool.onMouseDown(event, { canvas: canvasRef.current })
@@ -27,7 +13,13 @@ const ImageCamvasComponent = ({ imageSrc, activeTool }) => {
   }
 
   const handleMouseUp = (event) => {
-    if (activeTool) activeTool.onMouseUp(event, { canvas: canvasRef.current })
+    if (activeTool) {
+      activeTool.onMouseUp(event, { canvas: canvasRef.current, saveMemento })
+
+      const ctx = canvasRef.current.getContext('2d')
+      const canvasState = ctx.getImageData(0, 0, canvasRef.current.width, canvasRef.current.height)
+      saveMemento(mementoName, canvasState)
+    }
   }
 
   const handleMouseOut = (event) => {
@@ -37,6 +29,25 @@ const ImageCamvasComponent = ({ imageSrc, activeTool }) => {
   const handleRightClick = (event) => {
     if (activeTool) activeTool.onRightClick(event, { canvas: canvasRef.current })
   }
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const ctx = canvas.getContext('2d')
+
+    if (imageSrc && imageSrc.data) {
+      canvas.width = imageSrc.width
+      canvas.height = imageSrc.height
+      ctx.putImageData(imageSrc, 0, 0)
+    } else if (typeof imageSrc === 'string') {
+      const img = new Image()
+      img.src = imageSrc
+      img.onload = () => {
+        canvas.width = img.width
+        canvas.height = img.height
+        ctx.drawImage(img, 0, 0, img.width, img.height)
+      }
+    }
+  }, [imageSrc])
 
   return (
     <Container className='d-flex align-items-center justify-content-center min-vh-100'>
